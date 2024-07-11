95c48c30bd9bafff29842c24a6b163820d81e453
ALIRaza726 <rali80507@gmail.com>
2024-07-11 23:14:14 +0500

    convert List in menu button as a single variable

--- lib/models/menuBtn_provider.dart
+++ lib/models/menuBtn_provider.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
 class MenuButtonProvider with ChangeNotifier {
-  final List<String> _selectmenuvalue = [];
+   String _selectmenuvalue='';
 
-  List<String> get valuelist => _selectmenuvalue;
+  String get valuelist => _selectmenuvalue;
 
   void addvalue(String value ){
-    _selectmenuvalue.add(value);
+    _selectmenuvalue= value;
     notifyListeners();
   }
 
   void removevalue(String path) {
-    _selectmenuvalue.remove(path);
+    _selectmenuvalue=path;
     notifyListeners();
   }
 }

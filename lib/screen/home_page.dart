57638c5ea2ee18b6090e16bbe39f2dd2deb65a06
ALIRaza726 <rali80507@gmail.com>
2024-07-21 01:22:20 +0500

    setting getApi link + Page builder

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:sketch/models/gteApiLink_model.dart';
+import 'package:sketch/screen/gteApiLink.dart';
 import 'package:sketch/screen/app_bar.dart';
 import 'package:sketch/screen/bottom_navigation.dart';
 import 'package:sketch/screen/change_language.dart';
